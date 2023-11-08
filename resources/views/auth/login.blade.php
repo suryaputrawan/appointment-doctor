4de77882af19535aebbf3e97e5b69757f633bb7a
Ida Bagus Surya Putrawan <suryaputrawan@gmail.com>
2023-11-08 17:08:49 +0800

    Change Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,81 +1,91 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
-    
-<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:46 GMT -->
+	
+<!-- doccure/login.html  30 Nov 2019 04:12:20 GMT -->
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>Appointmet - Login</title>
+		<meta charset="utf-8">
+		<title>ADOS - Login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 		
-		<!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/admin/img/favicon.png') }}">
-
+		<!-- Favicons -->
+		<link href="{{ asset('assets/client/img/favicon.png') }}" rel="icon">
+		
 		<!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/admin/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('assets/client/css/bootstrap.min.css') }}">
 		
 		<!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/admin/css/font-awesome.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('assets/client/plugins/fontawesome/css/fontawesome.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('assets/client/plugins/fontawesome/css/all.min.css') }}">
 		
 		<!-- Main CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/admin/css/style.css') }}">
-		
-		<!--[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
-    </head>
-    <body>
+		<link rel="stylesheet" href="{{ asset('assets/client/css/style.css') }}">
 	
+	</head>
+	<body class="account-page">
+
 		<!-- Main Wrapper -->
-        <div class="main-wrapper login-body">
-            <div class="login-wrapper">
-            	<div class="container">
-                	<div class="loginbox">
-                    	<div class="login-left">
-							<img class="img-fluid" src="{{ asset('assets/admin/img/clinic_logo2.png') }}" alt="Logo">
-                        </div>
-                        <div class="login-right">
-							<div class="login-right-wrap">
-								<h1>Login</h1>
-								<p class="account-subtitle">Access to our dashboard</p>
-								
-								<!-- Form -->
-								<form action="{{ route('postlogin') }}" method="POST">
-                                    @csrf
-									<div class="form-group">
-										<input name="username" class="form-control @error('username') is-invalid @enderror" type="text" placeholder="Username">
-                                        @error('username')
-                                            <div class="invalid-feedback">{{ $message }}</div>
-                                        @enderror
+		<div class="main-wrapper">
+
+			<!-- Page Content -->
+			<div class="content">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-12 offset-md-12">
+							<!-- Login Tab Content -->
+							<div class="account-content">
+								<div class="row align-items-center justify-content-center">
+									<div class="col-md-7 col-lg-6 login-left">
+										<img src="{{ asset('assets/client/img/login-banner.png') }}" class="img-fluid" alt="login">	
 									</div>
-									<div class="form-group">
-										<input name="password" class="form-control @error('username') is-invalid @enderror" type="password" placeholder="Password">
-                                        @error('password')
-                                            <div class="invalid-feedback">{{ $message }}</div>
-                                        @enderror
+									<div class="col-md-12 col-lg-6 login-right">
+										<div class="login-header">
+											<h3>Login <span>ADOS</span></h3>
+										</div>
+										<form action="{{ route('postlogin') }}" method="POST">
+                                            @csrf
+											<div class="form-group form-focus">
+                                                <input name="username" class="form-control floating @error('username') is-invalid @enderror" type="text">
+												<label class="focus-label">Username</label>
+                                                @error('username')
+                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                @enderror
+											</div>
+											<div class="form-group form-focus">
+												<input name="password" class="form-control floating @error('username') is-invalid @enderror" type="password" placeholder="Password">
+                                                <label class="focus-label">Password</label>
+                                                @error('password')
+                                                    <div class="invalid-feedback">{{ $message }}</div>
+                                                @enderror
+											</div>
+											<div class="text-right">
+												<a class="forgot-link" href="#">Forgot Password ?</a>
+											</div>
+											<button class="btn btn-primary btn-block btn-lg login-btn" type="submit">Login</button>
+										</form>
 									</div>
-									<div class="form-group">
-										<button class="btn btn-primary btn-block" type="submit">Login</button>
-									</div>
-								</form>
-								<!-- /Form -->
+								</div>
 							</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+							<!-- /Login Tab Content -->
+						</div>
+					</div>
+				</div>
+			</div>		
+			<!-- /Page Content -->
+		   
+		</div>
 		<!-- /Main Wrapper -->
-		
+	  
 		<!-- jQuery -->
-        <script src="{{ asset('assets/admin/js/jquery-3.2.1.min.js') }}"></script>
+		<script src="{{ asset('assets/client/js/jquery.min.js') }}"></script>
 		
 		<!-- Bootstrap Core JS -->
-        <script src="{{ asset('assets/admin/js/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/admin/js/bootstrap.min.js') }}"></script>
+		<script src="{{ asset('assets/client/js/popper.min.js') }}"></script>
+		<script src="{{ asset('assets/client/js/bootstrap.min.js') }}"></script>
 		
 		<!-- Custom JS -->
-		<script src="{{ asset('assets/admin/js/script.js') }}"></script>
+		<script src="{{ asset('assets/client/js/script.js') }}"></script>
 		
-    </body>
+	</body>
+
+<!-- doccure/login.html  30 Nov 2019 04:12:20 GMT -->
 </html>
