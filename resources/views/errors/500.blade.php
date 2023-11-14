aee2a64d6fa56a186f42edcb93458a4b75d381ee
Ida Bagus Surya Putrawan <suryaputrawan@gmail.com>
2023-11-14 11:30:06 +0800

    Adding error pages

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>ADOS - Error 500</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/admin/img/favicon.png') }}">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/admin/css/bootstrap.min.css') }}">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/admin/css/font-awesome.min.css') }}">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/admin/css/feathericon.min.css') }}">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/admin/css/style.css') }}">
+		
+    </head>
+    <body class="error-page">
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+			
+			<div class="error-box">
+				<h1>500</h1>
+				<h3 class="h2 mb-3"><i class="fa fa-warning"></i> Oops! Page not found!</h3>
+				<p class="h4 font-weight-normal">Server Error</p>
+				<a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Back to Home</a>
+			</div>
+		
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="{{ asset('assets/admin/js/jquery-3.2.1.min.js') }}"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="{{ asset('assets/admin/js/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/admin/js/bootstrap.min.js') }}"></script>
+		
+		<!-- Custom JS -->
+		<script  src="{{ asset('assets/admin/js/script.js') }}"></script>
+		
+    </body>
+
+</html>
